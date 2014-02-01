0c0c7000468ad44a433646be64ea5849aaaf5d96
grendus <grendus@gmail.com>
2014-02-01 16:21:34 -0600

    Some new database methods

--- Database.py
+++ Database.py
@@ -6,8 +6,15 @@ class user(ndb.Model):
     password = ndb.StringProperty(indexed=False)
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
+
+    def query_user(self, username, password):
+        users = self.query(email_address=username)
+        if users.count()>0 and users.fetch()[0].password==password:
+            return True
+        return False
     
 class CADFile(ndb.Model):
+    submitter_name = ndb.StringProperty()
     filename = ndb.StringProperty()
     time = ndb.DateTimeProperty(auto_now_add=True)
     description = ndb.TextProperty()
@@ -15,7 +22,7 @@ class CADFile(ndb.Model):
     
 #checks the validity of every username/password combo
 def isValid(uname, pwd):
-    return True
+    return user.query_user(uname,pwd)
 
 #returns a list of dictionaries. Each one should have a filename, upload time, description, and status
 def getJobs(uname):
@@ -25,3 +32,10 @@ def getJobs(uname):
 def storeFile(ID, encFile, filedesc):
     pass
 
+#adds a user to the system; should only be done by the home system
+def addUser(email, encpwd, fname, lname):
+    newUser = user(email_address=email,
+                   password=encpwd,
+                   first_name=fname,
+                   last_name=lname)
+    newUser.put()

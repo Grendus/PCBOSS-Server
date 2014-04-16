4a2d0830e10a5e63d3072263b901faf1b7cbe299
grendus <grendus@gmail.com>
2014-04-15 23:11:34 -0500

    Changed encryption to use an envelope system instead. Can encrypt medium size files in seconds, should be good

--- Encryption.py
+++ Encryption.py
@@ -2,10 +2,15 @@ import random
 import keys
 import Crypto.PublicKey.RSA
 import Crypto.Hash.SHA512
-import base64
+from Crypto.Cipher import AES
+from Crypto import Random
 
-encryption_key = Crypto.PublicKey.RSA.importKey(keys.private_key)
-decryption_key = Crypto.PublicKey.RSA.importKey(keys.public_key)
+TOKEN_LENGTH = 16
+KEY_LENGTH = 32
+IV_LENGTH = 16
+
+encryption_key = Crypto.PublicKey.RSA.importKey(keys.public_key)
+decryption_key = Crypto.PublicKey.RSA.importKey(keys.private_key)
 
 def pwdHash(pwd):
     #todo: add hash function
@@ -14,66 +19,73 @@ def pwdHash(pwd):
     return hasher.hexdigest()
 
 def sessionToken():
-    #todo: implement secure session ID generator
-    sessionToken=""
-    for x in range(16):
-        sessionToken+=str(random.randint(0,9))
-    return sessionToken
+    return str(Random.new().read(TOKEN_LENGTH))
 
 """
 Messages can contain three different classes of value: dictionaries, lists, or values that 
 can be cast to string. This recursively parses the message, encrypting each part of the message
 while leaving the structure intact.
 """
+def encryptEntrance(unencryptedFile):
+	key = Random.new().read(KEY_LENGTH)
+	encFile = encrypt(unencryptedFile, key)
+	return {"key":encryption_key.encrypt(key,None)[0], "file":encFile}
 
-def encryptFile(unencryptedFile):
-    #todo: add encryption
-    return base64.encodestring(encryption_key.encrypt(str(unencryptedFile), None)[0])
+def encryptFile(unencryptedFile, key):
+	iv = Random.new().read(IV_LENGTH)
+	aes_cipher = AES.new(key, AES.MODE_CFB, iv)
+	message = iv+aes_cipher.encrypt(unencryptedFile)
+	return message
 
-def encryptDict(unencryptedDict):
+def encryptDict(unencryptedDict, encryptionkey):
 	encryptedDict = dict()
 	for key, value in unencryptedDict.iteritems():
-		encryptedDict[encrypt(key)] = encrypt(value)
+		encryptedDict[encrypt(key, encryptionkey)] = encrypt(value, encryptionkey)
 	return encryptedDict
 
-def encryptList(unencryptedList):
+def encryptList(unencryptedList, key):
 	encryptedList = list()
 	for value in unencryptedList:
-		encryptedList.append(encrypt(value))
+		encryptedList.append(encrypt(value, key))
 	return encryptedList
 
-def encrypt(unencryptedMessage):
+def encrypt(unencryptedMessage, key):
 	if type(unencryptedMessage) is dict:
-		return encryptDict(unencryptedMessage)
+		return encryptDict(unencryptedMessage, key)
 	elif type(unencryptedMessage) is list:
-		return encryptList(unencryptedMessage)
+		return encryptList(unencryptedMessage, key)
 	else:
-		return encryptFile(unencryptedMessage)
+		return encryptFile(unencryptedMessage, key)
 
 """
 Same as above, recursively decrypt the message sent. The logic is the same on both the
 server and local system, just different keys.
 """
+def decryptEntrance(encryptedFile):
+	key = decryption_key.decrypt(encryptedFile["key"])
+	return decrypt(encryptedFile["file"], key)
 
-def decryptFile(encryptedFile):
-	return decryption_key.decrypt(base64.decodestring(encryptedFile))
+def decryptFile(encryptedFile, key):
+	#generate an AES cipher using the initialization vector of the encrypted portion
+	aes_cipher = AES.new(key, AES.MODE_CFB, encryptedFile[:16])
+	return aes_cipher.decrypt(encryptedFile[16:])
 
-def decryptDict(encryptedDict):
+def decryptDict(encryptedDict, encryptionKey):
 	decryptedDict = dict()
 	for key, value in encryptedDict.iteritems():
-		decryptedDict[decrypt(key)] = decrypt(value)
+		decryptedDict[decrypt(key, encryptionKey)] = decrypt(value, encryptionKey)
 	return decryptedDict
 
-def decryptList(encryptedList):
+def decryptList(encryptedList, key):
 	decryptedList = list()
 	for value in encryptedList:
-		decryptedList.append(decrypt(value))
+		decryptedList.append(decrypt(value, key))
 	return decryptedList
 
-def decrypt(encryptedMessage):
+def decrypt(encryptedMessage, key):
 	if type(encryptedMessage) is dict:
-		return decryptDict(encryptedMessage)
+		return decryptDict(encryptedMessage, key)
 	elif type(encryptedMessage) is list:
-		return decryptList(encryptedMessage)
+		return decryptList(encryptedMessage, key)
 	else:
-		return decryptFile(encryptedMessage)
+		return decryptFile(encryptedMessage, key)
